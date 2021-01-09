9ba13705dfbcbae718dab7154485bbcc59db12ac
Cristian Deleon <libaration@gmail.com>
2021-01-09 12:16:39 -0500

    commit tree fe6e51a19610daa126d233fa08e63e3cdbfd43df

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1*num2;
 
-// Write your code here
+const random = Math.floor(Math.random()*10) + 1
+const mod = 4%7
+const max = Math.max(1,2,3,4,5,6,7,8,20)
